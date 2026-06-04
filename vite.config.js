032a819c0f9590c9b6f3bcc2e3c7acdbdafe36ef
1800adith <corppeep@gmail.com>
2026-06-04 16:42:53 -0700

    Set base path for Vite configuration
    Added base configuration for Vite to use relative paths.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: './', // 👈 Add this exact line!
+  plugins: [react()],
+})
   plugins: [react()],
   resolve: {
     alias: {
